3da0c5389da5e5ea954c940dca14ad7e4d720cb0
JK1234123 <146735002+JK1234123@users.noreply.github.com>
2024-04-23 11:15:54 +0200

    Added more tests

--- testy.py
+++ testy.py
@@ -50,108 +50,247 @@ class photosTest(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
 
-class get_postsTest(unittest.TestCase):
+class get_postsNotNoneTest(unittest.TestCase):
+    def test(self):
+        DataToTest = get_posts()
+        self.assertIsNotNone(DataToTest)
+
+
+class get_postsExpectedCountTest(unittest.TestCase):
     def test(self):
         url = "https://jsonplaceholder.typicode.com/posts"
         response = requests.get(url)
         ExpectedData = response.json()
         DataToTest = get_posts()
-        self.assertIsNotNone(DataToTest)
-        self.assertEqual(DataToTest,ExpectedData)
         self.assertCountEqual(DataToTest,ExpectedData)
 
 
-class get_post_by_idTest(unittest.TestCase):
+class get_postsExpectedDataTest(unittest.TestCase):
+    def test(self):
+        url = "https://jsonplaceholder.typicode.com/posts"
+        response = requests.get(url)
+        ExpectedData = response.json()
+        DataToTest = get_posts()
+        self.assertEqual(DataToTest,ExpectedData)
+
+
+class get_post_by_idNotNoneTest(unittest.TestCase):
+    def test(self):
+        global dane
+        get_posts()
+        dataToTest = get_post_by_id(23)
+        self.assertIsNotNone(dataToTest)
+
+
+class get_post_by_idExpectedTitleTest(unittest.TestCase):
     def test(self):
         ExpectedTitle = "maxime id vitae nihil numquam"
+        global dane
+        get_posts()
+        dataToTest = get_post_by_id(23)
+        self.assertEqual(dataToTest['title'],ExpectedTitle)
+
+
+class get_post_by_idExpectedUserIdTest(unittest.TestCase):
+    def test(self):
         ExpectedUserId = 3
+        global dane
+        get_posts()
+        dataToTest = get_post_by_id(23)
+        self.assertEqual(dataToTest['userId'], ExpectedUserId)
+
+
+class get_post_by_idExpectedIdTest(unittest.TestCase):
+    def test(self):
         ExpectedId = 23
+        global dane
+        get_posts()
+        dataToTest = get_post_by_id(23)
+        self.assertEqual(dataToTest['id'], ExpectedId)
+
+
+class get_post_by_idExpectedBodyTest(unittest.TestCase):
+    def test(self):
         ExpectedBody = "veritatis unde neque eligendi\nquae quod architecto quo neque vitae\nest illo sit tempora doloremque fugit quod\net et vel beatae sequi ullam sed tenetur perspiciatis"
         global dane
         get_posts()
         dataToTest = get_post_by_id(23)
-        EdgeCase1 = get_post_by_id(-1)
-        EdgeCase2 = get_post_by_id(99999)
-        self.assertIsNotNone(dataToTest)
-        self.assertEqual(dataToTest['title'],ExpectedTitle)
-        self.assertEqual(dataToTest['userId'], ExpectedUserId)
-        self.assertEqual(dataToTest['id'], ExpectedId)
         self.assertEqual(dataToTest['body'], ExpectedBody)
-        self.assertIsNone(EdgeCase1)
-        self.assertIsNone(EdgeCase2)
 
 
-class get_post_commentsTest(unittest.TestCase):
+class get_post_by_idNegativeInputTest(unittest.TestCase):
+    def test(self):
+        global dane
+        get_posts()
+        dataToTest = get_post_by_id(-1)
+        self.assertIsNone(dataToTest)
+
+
+class get_post_by_idVeryBigInputTest(unittest.TestCase):
+    def test(self):
+        global dane
+        get_posts()
+        dataToTest = get_post_by_id(99999)
+        self.assertIsNone(dataToTest)
+
+
+class get_post_commentsNotNoneTest(unittest.TestCase):
+    def test(self):
+        dataToTest = get_post_comments(23)
+        self.assertIsNotNone(dataToTest)
+
+
+class get_post_commentsExpectedCountTest(unittest.TestCase):
     def test(self):
         url = "https://jsonplaceholder.typicode.com/posts/23/comments"
         response = requests.get(url)
         ExpectedData = response.json()
         dataToTest = get_post_comments(23)
-        self.assertIsNotNone(dataToTest)
-        self.assertEqual(dataToTest, ExpectedData)
         self.assertCountEqual(dataToTest, ExpectedData)
 
 
-class get_limited_postsTest(unittest.TestCase):
+class get_post_commentsExpectedDataTest(unittest.TestCase):
+    def test(self):
+        url = "https://jsonplaceholder.typicode.com/posts/23/comments"
+        response = requests.get(url)
+        ExpectedData = response.json()
+        dataToTest = get_post_comments(23)
+        self.assertEqual(dataToTest, ExpectedData)
+
+
+class get_limited_postsNotNoneTest(unittest.TestCase):
+    def test(self):
+        dataToTest = get_limited_posts(10)
+        self.assertIsNotNone(dataToTest)
+
+
+class get_limited_postsExpectedDataTest(unittest.TestCase):
     def test(self):
         dataToTest = get_limited_posts(10)
         url = "https://jsonplaceholder.typicode.com/posts/"
         response = requests.get(url)
         ExpectedData = response.json()
-        EdgeCase1 = get_limited_posts(-9999)
-        EdgeCase2 = get_limited_posts(999999)
-        self.assertIsNotNone(dataToTest)
         self.assertEqual(dataToTest[0], ExpectedData[0])
-        self.assertEqual(len(dataToTest), 10)
-        self.assertEqual(EdgeCase1,[])
-        self.assertCountEqual(EdgeCase2, ExpectedData)
 
 
-class get_filtered_postsTest(unittest.TestCase):
+class get_limited_postsExpetedCountTest(unittest.TestCase):
     def test(self):
-        dataToTest = get_filtered_posts(10,120)
+        dataToTest = get_limited_posts(10)
+        self.assertEqual(len(dataToTest), 10)
+
+
+class get_limited_postsNegativeInputTest(unittest.TestCase):
+    def test(self):
+        dataToTest = get_limited_posts(-9999)
+        self.assertEqual(dataToTest,[])
+
+
+class get_limited_postsVeryBigInputTest(unittest.TestCase):
+    def test(self):
         url = "https://jsonplaceholder.typicode.com/posts/"
         response = requests.get(url)
         ExpectedData = response.json()
-        EdgeCase1 = get_filtered_posts(-9999, -120)
-        EdgeCase2 = get_filtered_posts(900000,999999)
-        self.assertIsNotNone(dataToTest)
-        self.assertEqual(len(dataToTest), 8)
-        self.assertEqual(EdgeCase1,[])
-        self.assertEqual(EdgeCase2,[])
+        dataToTest = get_limited_posts(999999)
+        self.assertCountEqual(dataToTest, ExpectedData)
 
-class get_albumsTest(unittest.TestCase):
+
+class get_filtered_postsNotNoneTest(unittest.TestCase):
+    def test(self):
+        dataToTest = get_filtered_posts(10,120)
+        self.assertIsNotNone(dataToTest)
+
+
+class get_filtered_postsExpectedCountTest(unittest.TestCase):
+    def test(self):
+        dataToTest = get_filtered_posts(10,120)
+        self.assertEqual(len(dataToTest), 8)
+
+
+class get_filtered_postsNegativeInputTest(unittest.TestCase):
+    def test(self):
+        dataToTest = get_filtered_posts(-9999, -120)
+        self.assertEqual(dataToTest,[])
+
+
+class get_filtered_postsVeryBigInputTest(unittest.TestCase):
+    def test(self):
+        dataToTest = get_filtered_posts(900000,999999)
+        self.assertEqual(dataToTest,[])
+
+
+class get_albumsNotNoneTest(unittest.TestCase):
+    def test(self):
+        dataToTest = get_albums()
+        self.assertIsNotNone(dataToTest)
+
+
+class get_albumsExpectedCountTest(unittest.TestCase):
     def test(self):
         url = "https://jsonplaceholder.typicode.com/albums"
         response = requests.get(url)
         ExpectedData = response.json()
         dataToTest = get_albums()
-        self.assertIsNotNone(dataToTest)
-        self.assertEqual(dataToTest,ExpectedData)
         self.assertCountEqual(dataToTest,ExpectedData)
 
 
-class get_album_by_idTest(unittest.TestCase):
+class get_albumsExpectedDataTest(unittest.TestCase):
     def test(self):
-        ExpectedTitle = "incidunt quisquam hic adipisci sequi"
-        ExpectedUserId = 3
-        ExpectedId = 23
+        url = "https://jsonplaceholder.typicode.com/albums"
+        response = requests.get(url)
+        ExpectedData = response.json()
+        dataToTest = get_albums()
+        self.assertEqual(dataToTest,ExpectedData)
+
+
+class get_album_by_idNotNoneTest(unittest.TestCase):
+    def test(self):
         dataToTest = get_album_by_id(23)
         self.assertIsNotNone(dataToTest)
+
+
+class get_album_by_idExpectedTitleTest(unittest.TestCase):
+    def test(self):
+        ExpectedTitle = "incidunt quisquam hic adipisci sequi"
+        dataToTest = get_album_by_id(23)
         self.assertEqual(dataToTest['title'],ExpectedTitle)
+
+
+class get_album_by_idExpectedUserIdTest(unittest.TestCase):
+    def test(self):
+        ExpectedUserId = 3
+        dataToTest = get_album_by_id(23)
         self.assertEqual(dataToTest['userId'], ExpectedUserId)
+
+
+class get_album_by_idExpectedIdTest(unittest.TestCase):
+    def test(self):
+        ExpectedId = 23
+        dataToTest = get_album_by_id(23)
         self.assertEqual(dataToTest['id'], ExpectedId)
 
 
-class get_photosTest(unittest.TestCase):
+class get_photosNotNoneTest(unittest.TestCase):
+    def test(self):
+        dataToTest = get_photos(23)
+        self.assertIsNotNone(dataToTest)
+
+
+class get_photosExpectedCountTest(unittest.TestCase):
     def test(self):
         url = "https://jsonplaceholder.typicode.com/albums/23/photos"
         response = requests.get(url)
         ExpectedData = response.json()
         dataToTest = get_photos(23)
-        self.assertIsNotNone(dataToTest)
-        self.assertEqual(dataToTest, ExpectedData)
         self.assertCountEqual(dataToTest, ExpectedData)
+
+
+class get_photosExpectedDataTest(unittest.TestCase):
+    def test(self):
+        url = "https://jsonplaceholder.typicode.com/albums/23/photos"
+        response = requests.get(url)
+        ExpectedData = response.json()
+        dataToTest = get_photos(23)
+        self.assertEqual(dataToTest, ExpectedData)
 
 
 if __name__ == '__main__':
