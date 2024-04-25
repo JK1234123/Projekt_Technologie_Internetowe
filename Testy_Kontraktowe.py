ef9f98068d961060d4b93fb1ef9df2a2724f2e14
Jakub Kapłon <jakub.kaplon1@edu.uekat.pl>
2024-04-25 22:04:36 +0200

    Fixed Kontrakt tests

--- Testy_Kontraktowe.py
+++ Testy_Kontraktowe.py
@@ -2,18 +2,32 @@ import requests
 import json
 import pytest
 
-@pytest.mark.contract
-def test_get_user_contract():
-    expected_user_data = [{
-    "userId": 1,
-    "id": 1,
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
-    }]
+
+def test_get_response():
     url = "http://127.0.0.1:5000/api"
     params = {"limit": int(1)}
     response = requests.get(url, params)
     assert response.status_code == 200
-    assert response.headers["Content-Type"] == "application/json"
+    # assert response.headers["Content-Type"] == "application/json"
+    # user_data = response.json()
+    # assert user_data == expected_user_data
+
+
+def test_data():
+    expected_user_data = [{
+        "userId": 1,
+        "id": 1,
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+    }]
+    url = "http://127.0.0.1:5000/api"
+    params = {"limit": int(1)}
+    response = requests.get(url, params)
     user_data = response.json()
     assert user_data == expected_user_data
+
+def test_response_type():
+    url = "http://127.0.0.1:5000/api"
+    params = {"limit": int(1)}
+    response = requests.get(url, params)
+    assert response.headers["Content-Type"] == "application/json"
